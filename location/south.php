125cb89a5aa5e955cecbedea20192658c346fb0a
yf-codezz <y.fujita@codezz.net>
2023-05-10 17:58:58 +0900

    locaiton/南区エリアimg追加

--- location/south.php
+++ location/south.php
@@ -14,7 +14,7 @@
         <div class="area-section__top-photo">
             <picture class="area-section__photo">
                 <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-01-01.webp">
-                <img src="<?php print URL; ?>/assets/img/location/area-01-01.jpg" alt="大通公園" class="area-section__img">
+                <img src="<?php print URL; ?>/assets/img/location/area-01-01.jpg" alt="" class="area-section__img">
             </picture>
             <!-- <p class="area-section__photo-title">大通公園</p> -->
         </div>
@@ -23,21 +23,28 @@
                 <li class="area-section__photo-item">
                     <picture class="area-section__photo">
                         <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-01-02.webp">
-                        <img src="<?php print URL; ?>/assets/img/location/area-01-02.jpg" alt="すすきの" class="area-section__img">
+                        <img src="<?php print URL; ?>/assets/img/location/area-01-02.jpg" alt="" class="area-section__img">
                     </picture>
                     <!-- <p class="area-section__photo-title">すすきの</p> -->
                 </li>
                 <li class="area-section__photo-item">
                     <picture class="area-section__photo">
                         <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-01-03.webp">
-                        <img src="<?php print URL; ?>/assets/img/location/area-01-03.jpg" alt="札幌ステラプレイス" class="area-section__img">
+                        <img src="<?php print URL; ?>/assets/img/location/area-01-03.jpg" alt="" class="area-section__img">
                     </picture>
                     <!-- <p class="area-section__photo-title">札幌ステラプレイス</p> -->
                 </li>
                 <li class="area-section__photo-item">
                     <picture class="area-section__photo">
                         <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-01-04.webp">
-                        <img src="<?php print URL; ?>/assets/img/location/area-01-04.jpg" alt="大丸" class="area-section__img">
+                        <img src="<?php print URL; ?>/assets/img/location/area-01-04.jpg" alt="" class="area-section__img">
+                    </picture>
+                    <!-- <p class="area-section__photo-title">大丸</p> -->
+                </li>
+                <li class="area-section__photo-item">
+                    <picture class="area-section__photo">
+                        <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-01-05.webp">
+                        <img src="<?php print URL; ?>/assets/img/location/area-01-05.jpg" alt="" class="area-section__img">
                     </picture>
                     <!-- <p class="area-section__photo-title">大丸</p> -->
                 </li>
@@ -55,24 +62,32 @@
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-02-01.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-02-01.jpg" alt="北海道近代美術館" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-02-01.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">北海道近代美術館</p> -->
             </li>
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-02-02.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-02-02.jpg" alt="札幌医科大学付属病院" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-02-02.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">札幌医科大学付属病院</p> -->
             </li>
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-02-03.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-02-03.jpg" alt="札幌市教育文化会館" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-02-03.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">札幌市教育文化会館</p> -->
             </li>
+            <li class="area-section__photo-item">
+                <picture class="area-section__photo">
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-02-04.webp">
+                    <img src="<?php print URL; ?>/assets/img/location/area-02-04.jpg" alt="" class="area-section__img">
+                </picture>
+                <!-- <p class="area-section__photo-title">札幌市教育文化会館</p> -->
+            </li>
+
         </ul>
         <div class="area-section__text-area">
             <h3 class="area-section__title">札幌芸術の森</h3>
@@ -84,24 +99,32 @@
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-03-01.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-03-01.jpg" alt="旭山記念公園" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-03-01.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">旭山記念公園</p> -->
             </li>
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-03-02.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-03-02.jpg" alt="DCMホーマック旭ヶ丘店" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-03-02.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">DCMホーマック旭ヶ丘店</p> -->
             </li>
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-03-03.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-03-03.jpg" alt="西友旭ヶ丘店" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-03-03.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">西友旭ヶ丘店</p> -->
             </li>
+            <li class="area-section__photo-item">
+                <picture class="area-section__photo">
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-03-04.webp">
+                    <img src="<?php print URL; ?>/assets/img/location/area-03-04.jpg" alt="" class="area-section__img">
+                </picture>
+                <!-- <p class="area-section__photo-title">西友旭ヶ丘店</p> -->
+            </li>
+
         </ul>
         <div class="area-section__text-area">
             <h3 class="area-section__title">石山緑地</h3>
@@ -113,24 +136,32 @@
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-04-01.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-04-01.jpg" alt="藻岩山ロープウェイ" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-04-01.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">藻岩山ロープウェイ</p> -->
             </li>
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-04-02.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-04-02.jpg" alt="藻岩山スキー場" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-04-02.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">藻岩山スキー場</p> -->
             </li>
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-04-03.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-04-03.jpg" alt="円山藻岩山ハイキングルート" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-04-03.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">円山藻岩山ハイキングルート</p> -->
             </li>
+            <li class="area-section__photo-item">
+                <picture class="area-section__photo">
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-04-04.webp">
+                    <img src="<?php print URL; ?>/assets/img/location/area-04-04.jpg" alt="" class="area-section__img">
+                </picture>
+                <!-- <p class="area-section__photo-title">円山藻岩山ハイキングルート</p> -->
+            </li>
+
         </ul>
         <div class="area-section__text-area">
             <h3 class="area-section__title">藻岩山</h3>
@@ -142,28 +173,28 @@
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-05-01.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-05-01.jpg" alt="アクロスプラザ南22条店" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-05-01.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">アクロスプラザ南22条店</p> -->
             </li>
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-05-02.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-05-02.jpg" alt="コープさっぽろやまはな店" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-05-02.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">コープさっぽろやまはな店</p> -->
             </li>
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-05-03.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-05-03.jpg" alt="札幌市中央図書館" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-05-03.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">札幌市中央図書館</p> -->
             </li>
             <li class="area-section__photo-item">
                 <picture class="area-section__photo">
                     <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/location/area-05-04.webp">
-                    <img src="<?php print URL; ?>/assets/img/location/area-05-04.jpg" alt="東光ストアプロム山鼻店" class="area-section__img">
+                    <img src="<?php print URL; ?>/assets/img/location/area-05-04.jpg" alt="" class="area-section__img">
                 </picture>
                 <!-- <p class="area-section__photo-title">東光ストアプロム山鼻店</p> -->
             </li>

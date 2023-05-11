f8e9938e3fb1d50cfac22dc98673d1a624c61675
HoshidaKazuki <k.hoshida@codezz.net>
2023-05-11 17:06:12 +0900

    エクステリアエラー修正

--- exterior/content.php
+++ exterior/content.php
@@ -2,15 +2,15 @@
     <h2 class="title-h2 title-h2_exterior fadeUpTrigger">エクステリア</h2>
     <section class="tab-section tab-section_column european-grace">
         <h2 class="european-grace__h2 flipRightTrigger">
-          <picture class="european-grace__photo">
-            <source media="(min-width: 768px)" type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/img-01_pc.webp">
-            <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/img-01_sp.webp">
-            <source media="(min-width: 768px)" type="image/png" srcset="<?php print URL; ?>/assets/img/exterior/img-01_sp.png">
-            <img src="<?php print URL; ?>/assets/img/exterior/img-01_sp.png" alt="european grace" class="european-grace__img">
-          </picture>
+            <picture class="european-grace__photo">
+                <source media="(min-width: 768px)" type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/img-01_pc.webp">
+                <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/img-01_sp.webp">
+                <source media="(min-width: 768px)" type="image/png" srcset="<?php print URL; ?>/assets/img/exterior/img-01_sp.png">
+                <img src="<?php print URL; ?>/assets/img/exterior/img-01_sp.png" alt="european grace" class="european-grace__img">
+            </picture>
         </h2>
         <p class="text european-grace__text fadeUpTrigger">
-          ヨーロッパの伝統的な建築物の内部に足を踏み入れると、そこに漂うのは凛とした空気。それを大切にしたくて、意匠のディティールにはヨーロピアンテイストを随所に散りばめました。ヨーロッパの優れた建造物が多くの人を魅了するのは、意匠に対する理想と思想に妥協を許さず、時代性や文化、伝統の先進性を意識しつつ、環境にも配慮したデザインが背景にあるからです。
+            ヨーロッパの伝統的な建築物の内部に足を踏み入れると、そこに漂うのは凛とした空気。それを大切にしたくて、意匠のディティールにはヨーロピアンテイストを随所に散りばめました。ヨーロッパの優れた建造物が多くの人を魅了するのは、意匠に対する理想と思想に妥協を許さず、時代性や文化、伝統の先進性を意識しつつ、環境にも配慮したデザインが背景にあるからです。
         </p>
     </section>
     <section class="tab-section">
@@ -23,7 +23,7 @@
             </div>
         </div>
         <div class="intro intro_massage fadeUpTrigger delay-time06">
-            <p class="section__lead">欧風建築の美学を求めて。<br>迎賓と安らぎの日常へ、ようこそ。</p>
+            <h3 class="section__lead">欧風建築の美学を求めて。<br>迎賓と安らぎの日常へ、ようこそ。</h3>
             <p class="text">
                 そこに、想像を遥かに超える別世界が幕を開けようとしています。<br>
                 ご提案するのは、時を越え、誇りの象徴であり続ける頂の地。<br>
@@ -35,83 +35,83 @@
     <section class="tab-section euro-arch">
         <h2 class="section__lead section__lead_euro-arch fadeUpTrigger">欧風建築の美学をこの地に再現。</h2>
         <picture class="euro-arch__photo rotateYTrigger">
-          <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/img-06.webp">
-          <img src="<?php print URL; ?>/assets/img/exterior/img-06.jpg" alt="欧風建築の美学をこの地に再現。" class="euro-arch__photo-img">
+            <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/img-06.webp">
+            <img src="<?php print URL; ?>/assets/img/exterior/img-06.jpg" alt="欧風建築の美学をこの地に再現。" class="euro-arch__photo-img">
         </picture>
         <p class="text euro-arch__text fadeUpTrigger">外観は、太陽や青空に映え、周囲の街並みとも美しく調和するブラウン調の磁器質タイルをベースにし、グレーカラーで上質なアクセントを加えました。建物の随所に、ヨーロッパ建築のデザイン・モチーフが活かされていることも大きな特徴。時代の流れに磨かれて、なお輝きを増していくヨーロッパの建築物。その根底には、建築は街の景観美を創造することであるという確固とした思想が息づいています。</p>
         <ul class="euro-arch__photo-list">
-          <li class="euro-arch__item fadeInTrigger">
-            <picture>
-              <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-01.webp">
-              <img src="<?php print URL; ?>/assets/img/exterior/ea-01.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
-            </picture>
-          </li>
-          <li class="euro-arch__item fadeInTrigger delay-time03">
-            <picture>
-              <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-02.webp">
-              <img src="<?php print URL; ?>/assets/img/exterior/ea-02.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
-            </picture>
-          </li>
-          <li class="euro-arch__item fadeInTrigger delay-time04">
-            <picture>
-              <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-03.webp">
-              <img src="<?php print URL; ?>/assets/img/exterior/ea-03.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
-            </picture>
-          </li>
-          <li class="euro-arch__item fadeInTrigger delay-time05">
-            <picture>
-              <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-04.webp">
-              <img src="<?php print URL; ?>/assets/img/exterior/ea-04.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
-            </picture>
-          </li>
-          <li class="euro-arch__item fadeInTrigger delay-time06">
-            <picture>
-              <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-05.webp">
-              <img src="<?php print URL; ?>/assets/img/exterior/ea-05.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
-            </picture>
-          </li>
-          <li class="euro-arch__item fadeInTrigger delay-time07">
-            <picture>
-              <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-06.webp">
-              <img src="<?php print URL; ?>/assets/img/exterior/ea-06.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
-            </picture>
-          </li>
-          <li class="euro-arch__item fadeInTrigger delay-time08">
-            <picture>
-              <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-07.webp">
-              <img src="<?php print URL; ?>/assets/img/exterior/ea-07.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
-            </picture>
-          </li>
-          <li class="euro-arch__item fadeInTrigger delay-time09">
-            <picture>
-              <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-08.webp">
-              <img src="<?php print URL; ?>/assets/img/exterior/ea-08.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
-            </picture>
-          </li>
+            <li class="euro-arch__item fadeInTrigger">
+                <picture>
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-01.webp">
+                    <img src="<?php print URL; ?>/assets/img/exterior/ea-01.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
+                </picture>
+            </li>
+            <li class="euro-arch__item fadeInTrigger delay-time03">
+                <picture>
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-02.webp">
+                    <img src="<?php print URL; ?>/assets/img/exterior/ea-02.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
+                </picture>
+            </li>
+            <li class="euro-arch__item fadeInTrigger delay-time04">
+                <picture>
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-03.webp">
+                    <img src="<?php print URL; ?>/assets/img/exterior/ea-03.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
+                </picture>
+            </li>
+            <li class="euro-arch__item fadeInTrigger delay-time05">
+                <picture>
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-04.webp">
+                    <img src="<?php print URL; ?>/assets/img/exterior/ea-04.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
+                </picture>
+            </li>
+            <li class="euro-arch__item fadeInTrigger delay-time06">
+                <picture>
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-05.webp">
+                    <img src="<?php print URL; ?>/assets/img/exterior/ea-05.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
+                </picture>
+            </li>
+            <li class="euro-arch__item fadeInTrigger delay-time07">
+                <picture>
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-06.webp">
+                    <img src="<?php print URL; ?>/assets/img/exterior/ea-06.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
+                </picture>
+            </li>
+            <li class="euro-arch__item fadeInTrigger delay-time08">
+                <picture>
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-07.webp">
+                    <img src="<?php print URL; ?>/assets/img/exterior/ea-07.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
+                </picture>
+            </li>
+            <li class="euro-arch__item fadeInTrigger delay-time09">
+                <picture>
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/ea-08.webp">
+                    <img src="<?php print URL; ?>/assets/img/exterior/ea-08.jpg" alt="欧風建築イメージ画像" class="euro-arch__photo-img">
+                </picture>
+            </li>
         </ul>
         <div class="section__inner-wrapper">
-          <section class="euro-arch__section">
-            <picture class="euro-arch__section-photo fadeLeftTrigger">
-              <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/img-07.webp">
-              <img src="<?php print URL; ?>/assets/img/exterior/img-07.jpg" alt="お届けしたいのは、ヨーロッパ建築の伝統が香るアーティスティックなデザイン。" class="euro-arch__photo-img">
-            </picture>
-            <div class="euro-arch__text-area fadeRightTrigger">
-              <h3 class="title-h3">お届けしたいのは、<br>
-                ヨーロッパ建築の伝統が香る<br>
-                アーティスティックなデザイン。</h3>
-              <p class="text">ヨーロッパ建築の持つ様式美や普遍性、品格を継承し、さらに独自の発想と技術で、新しい居住スタイルを創造すること。私たちがお届けする「グランファーレ真駒内グランフォレスト」には、常にこのコンセプトが息づいています。合理性や流行だけに左右されず、都市景観と融合する上質な美しさを、その佇まいは雄弁に語ります。</p>
-            </div>
-          </section>
-          <section class="euro-arch__section">
-            <picture class="euro-arch__section-photo fadeRightTrigger">
-              <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/img-08.webp">
-              <img src="<?php print URL; ?>/assets/img/exterior/img-08.jpg" alt="格調とモダンが調和する憧憬邸宅。" class="euro-arch__photo-img">
-            </picture>
-            <div class="euro-arch__text-area fadeLeftTrigger">
-              <h3 class="title-h3">格調とモダンが調和する憧憬邸宅。</h3>
-              <p class="text">伝統を感じさせる作品は、時代を超えてもなお、その輝きを増すもの。そして、いつの時代においても瑞々しい感性と響き合う。この歴史ある「真駒内」の地に立つ「グランファーレ真駒内グランフォレスト」は、どこまでも気高く、いつまでも愛され続ける住まいを目指します。</p>
-            </div>
-          </section>
+            <section class="euro-arch__section">
+                <picture class="euro-arch__section-photo fadeLeftTrigger">
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/img-07.webp">
+                    <img src="<?php print URL; ?>/assets/img/exterior/img-07.jpg" alt="お届けしたいのは、ヨーロッパ建築の伝統が香るアーティスティックなデザイン。" class="euro-arch__photo-img">
+                </picture>
+                <div class="euro-arch__text-area fadeRightTrigger">
+                    <h3 class="title-h3">お届けしたいのは、<br>
+                        ヨーロッパ建築の伝統が香る<br>
+                        アーティスティックなデザイン。</h3>
+                    <p class="text">ヨーロッパ建築の持つ様式美や普遍性、品格を継承し、さらに独自の発想と技術で、新しい居住スタイルを創造すること。私たちがお届けする「グランファーレ真駒内グランフォレスト」には、常にこのコンセプトが息づいています。合理性や流行だけに左右されず、都市景観と融合する上質な美しさを、その佇まいは雄弁に語ります。</p>
+                </div>
+            </section>
+            <section class="euro-arch__section">
+                <picture class="euro-arch__section-photo fadeRightTrigger">
+                    <source type="image/webp" srcset="<?php print URL; ?>/assets/webp/exterior/img-08.webp">
+                    <img src="<?php print URL; ?>/assets/img/exterior/img-08.jpg" alt="格調とモダンが調和する憧憬邸宅。" class="euro-arch__photo-img">
+                </picture>
+                <div class="euro-arch__text-area fadeLeftTrigger">
+                    <h3 class="title-h3">格調とモダンが調和する憧憬邸宅。</h3>
+                    <p class="text">伝統を感じさせる作品は、時代を超えてもなお、その輝きを増すもの。そして、いつの時代においても瑞々しい感性と響き合う。この歴史ある「真駒内」の地に立つ「グランファーレ真駒内グランフォレスト」は、どこまでも気高く、いつまでも愛され続ける住まいを目指します。</p>
+                </div>
+            </section>
         </div>
     </section>
 

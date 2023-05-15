96a8257266389d8080bdc4ed96b79d096defa105
HoshidaKazuki <k.hoshida@codezz.net>
2023-05-15 09:55:55 +0900

    TOPアニメーション修正

--- top/index.php
+++ top/index.php
@@ -54,24 +54,24 @@
                 <span class="lead-one lead-one_9">出</span>
                 <span class="lead-one lead-one_10">し</span>
                 <span class="lead-one lead-one_11">、</span><br class="br_sp">
-                <span class="lead-one lead-one_12">街</span>
-                <span class="lead-one lead-one_13">の</span>
-                <span class="lead-one lead-one_14">景</span>
-                <span class="lead-one lead-one_15">観</span>
-                <span class="lead-one lead-one_1">に</span>
-                <span class="lead-one lead-one_2">も</span>
-                <span class="lead-one lead-one_3">調</span>
-                <span class="lead-one lead-one_4">和</span>
-                <span class="lead-one lead-one_5">す</span>
-                <span class="lead-one lead-one_6">る</span>
-                <span class="lead-one lead-one_7">低</span>
-                <span class="lead-one lead-one_8">層</span>
-                <span class="lead-one lead-one_9">レ</span>
-                <span class="lead-one lead-one_10">ジ</span>
-                <span class="lead-one lead-one_11">デ</span>
-                <span class="lead-one lead-one_12">ン</span>
-                <span class="lead-one lead-one_13">ス</span>
-                <span class="lead-one lead-one_14">。</span>
+                <span class="lead-one lead-one_13">街</span>
+                <span class="lead-one lead-one_14">の</span>
+                <span class="lead-one lead-one_15">景</span>
+                <span class="lead-one lead-one_16">観</span>
+                <span class="lead-one lead-one_17">に</span>
+                <span class="lead-one lead-one_18">も</span>
+                <span class="lead-one lead-one_19">調</span>
+                <span class="lead-one lead-one_20">和</span>
+                <span class="lead-one lead-one_21">す</span>
+                <span class="lead-one lead-one_22">る</span>
+                <span class="lead-one lead-one_23">低</span>
+                <span class="lead-one lead-one_24">層</span>
+                <span class="lead-one lead-one_25">レ</span>
+                <span class="lead-one lead-one_26">ジ</span>
+                <span class="lead-one lead-one_27">デ</span>
+                <span class="lead-one lead-one_28">ン</span>
+                <span class="lead-one lead-one_29">ス</span>
+                <span class="lead-one lead-one_30">。</span>
             </p>
         </div>
     </div>
@@ -89,11 +89,11 @@
                 <div><span class="feature__item_normal">オープンペントハウス付き</span>タイプ、</div>
                 <div><span class="feature__item_normal">専用庭</span>・<span class="feature__item_normal">テラス付き</span>タイプをご用意。</div>
             </li>
-            <li class="feature__item fadeInTrigger delay-time12">
+            <li class="feature__item fadeInTrigger delay-time21">
                 <div>不凍散水栓「多目的スロップシンク」採用で、</div>
                 <div><span class="feature__item_normal">ガーデニングも快適</span>です。</div>
             </li>
-            <li class="feature__item fadeInTrigger delay-time15">
+            <li class="feature__item fadeInTrigger delay-time24">
                 <div><span class="feature__item_normal">真駒内駅前</span>のバス乗り場から</div>
                 <div><span class="feature__item_normal">新千歳空港</span>や市内各所へ快適アクセス。</div>
             </li>

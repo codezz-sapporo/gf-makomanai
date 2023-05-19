77031879063e01299262ff2b621332fd48692c97
HoshidaKazuki <k.hoshida@codezz.net>
2023-05-19 16:07:36 +0900

    間取りBタイプ修正

---
+++ floor-plan/b-detail.php
@@ -0,0 +1,39 @@
+<div id="menuplan">
+                    <h4>Menu Plan</h4>
+                    <div class="menu">
+                        <dl>
+                            <dt class="fgara">menu<span>1</span></dt>
+                            <dd>リビングダイニングと洋室3の間に間仕切り壁を設けてプライバシーを確保しました。 </dd>
+                        </dl>
+                        <p class="floorimg ">
+                            <img src="../assets/svg/a-type/b-type_menu1.svg" width="630" height="615" alt="menu1">
+                        </p>
+                    </div>
+                    <div class="menu">
+                        <dl>
+                            <dt class="fgara">menu<span>2</span></dt>
+                            <dd>リビングダイニングに洋室3を取り込んだ2LDKプランです。19.3帖大のリビングダイニングを実現しました。 </dd>
+                        </dl>
+                        <p class="floorimg">
+                            <img src="../assets/svg/a-type/b-type_menu2.svg" width="630" height="615" alt="menu2">
+                        </p>
+                    </div>
+                    <div class="menu">
+                        <dl>
+                            <dt class="fgara">menu<span>3</span></dt>
+                            <dd>洋室1と洋室2を1部屋にした2LDKプランです。洋室のクローゼットをウォークインに変更、ホール収納を追加し収納スペースを確保しました。 </dd>
+                        </dl>
+                        <p class="floorimg">
+                            <img src="../assets/svg/a-type/b-type_menu3.svg" width="630" height="615" alt="menu3">
+                        </p>
+                    </div>
+                    <div class="menu">
+                        <dl>
+                            <dt class="fgara">menu<span>4</span></dt>
+                            <dd>洋室1と洋室2を1部屋にした2LDKプランです。洋室のクローゼットをウォークインに変更、もう一つのクローゼットも広く設けて収納スペースを確保しました。 </dd>
+                        </dl>
+                        <p class="floorimg">
+                            <img src="../assets/svg/a-type/b-type_menu4.svg" width="630" height="615" alt="menu4">
+                        </p>
+                    </div>
+                </div>
